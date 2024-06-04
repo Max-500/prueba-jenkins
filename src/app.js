b7da5374dd80a9ebc9ef3820e918f9da0d7cd1f8
Max-500 <213497@ids.upchiapas.edu.mx>
2024-06-03 21:12:17 -0600

    das

--- src/app.js
+++ src/app.js
@@ -9,17 +9,17 @@ app.use(cors());
 const port = process.env.PORT || 3000;
 
 app.get('/hello', (req, res) => {
-    res.send("Hello World")
+    res.send("Hello World");
 });
 
 app.get('/jenkins', (req, res) => {
-    res.send("Hello Jenkins")
+    res.send("Hello Jenkins");
 });
 
 app.get('/bye', (req, res) => {
-    res.send("Bye World")
+    res.send("Bye World");
 });
 
 app.listen(port, () => {
-    console.log(`SERVER RUNNING IN http://localhost:${port}.`)
+    console.log(`SERVER RUNNING IN http://localhost:${port}.`);
 });
