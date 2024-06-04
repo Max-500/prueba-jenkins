a3aa1bf4e7fbcc2da9aa3308dfecb028da251da0
Max-500 <213497@ids.upchiapas.edu.mx>
2024-06-03 20:21:35 -0600

    Prueba final

--- src/app.js
+++ src/app.js
@@ -12,6 +12,14 @@ app.get('/hello', (req, res) => {
     res.send("Hello World");
 });
 
+app.get('/jenkins', (req, res) => {
+    res.send("Hello Jenkins");
+});
+
+app.get('/bye', (req, res) => {
+    res.send("Bye World");
+});
+
 app.listen(port, () => {
     console.log(`SERVER RUNNING IN http://localhost:${port}.`);
 });
